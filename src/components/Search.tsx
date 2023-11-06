8f504cc947755cfe2e9a3ff9b27f5ca4197839b0
Bhupinder Singh <bs082045@gmail.com>
2023-11-06 16:05:16 -0500

    fix header ui & search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,139 @@
+"use client"
+
+import Link from 'next/link';
+import { Component } from 'react';
+
+async function searchDishes(query: string) {
+
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json();
+
+};
+
+
+type SearchState = {
+    query: string,
+    recipes: any
+}
+
+
+class Search extends Component<{}, SearchState> {
+
+    constructor(props: any) {
+
+        super(props);
+        this.state = {
+            query: '',
+            recipes: null
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+
+    }
+
+    handleClick() {
+
+        this.setState({ query: '', recipes: [] });
+    }
+
+
+    handleChange(event: any) {
+
+        event.preventDefault();
+
+        this.setState({ query: event.target.value }, this.handleSearch);
+    }
+
+    async handleSearch(){
+
+        
+        if (!this.state.query.length) { 
+            return null;
+        }
+                
+        const data = await searchDishes(this.state.query);
+        this.setState({ recipes: data.meals });
+        
+    }
+
+
+
+
+    render() {
+
+        let classes = 'shadow w-full rounded text-left absolute bg-white mt-2';
+
+        return (<div className="relative">
+            <svg className="absolute left-0 top-0 mx-3 mt-2 fill-current text-gray-400"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 
+					16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
+            </svg>
+            <form>
+                <input onChange={this.handleChange}
+                    value={this.state.query} type="search" placeholder="Pasta..."
+                    className="block w-full border px-10 py-2 rounded-md focus:outline-none focus:shadow-sm" />
+            </form>
+            
+
+           {
+            this.state.query &&
+            <SuggestionBox recipes={this.state.recipes} classes={classes} handleClick={this.handleClick} />
+            }
+        </div>);
+
+    }
+
+}
+
+
+function SuggestionBox(props: any) {
+
+    if(!props.recipes){
+
+        return <ul className={props.classes}>
+        {  
+            <li className="px-2 py-1 text-gray-800 hover:bg-gray-100">
+                <p className="text-gray-800 ml-4">No such recipe</p>
+            </li> 
+        }
+        </ul>;
+    
+    }
+
+    return <ul className={props.classes}>
+        { props.recipes.map((recipe : any) => (
+            <li key={recipe.idMeal} className="px-2 py-1 text-gray-800 hover:bg-gray-100" onClick={props.handleClick}>
+            <Link href="/recipe/[id]" as={`/recipe/${recipe.idMeal}`} className="flex items-center">
+                <img src={recipe.strMealThumb} alt="image" className="w-12 h-12 rounded" />
+                <p className="text-gray-800 ml-4">{recipe.strMeal}</p>
+            </Link>
+        </li>)) }
+    </ul>;
+
+}
+
+
+
+
+export default Search;
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,34 @@
 import Link from "next/link";
+import Search from "./Search";
 
 const Header = () => {
-	
-    return( <header className="flex flex-wrap items-center py-2 md:py-4 border-b">
-             <nav className="w-3/12 px-2 md:px-4 tracking-wider
-             font-semibold text-gray-800">
-                 <Link href="/" className="px-2 flex items-center justify-center">
-                    <img src="/img/hat.svg" alt="logo"
-                    className="w-12 h-12 object-cover inline-block" />
-                    <h2 className="ml-1 text-xl md:text-3xl tracking-wider
-                    text-green-600 tracking-wider hidden sm:inline-block my-font">foodie</h2>			  
-                 </Link>
-             </nav>
-             
-            <div className="w-9/12 md:w-6/12 px-2 md:px-4">
-            { /* search bar */ }
-            </div>
+
+   
+   return( <header className="flex flex-wrap items-center py-3 border-b">
+            <nav className="w-3/12 px-2 md:px-4 tracking-wider
+            font-semibold text-gray-800">
+               <Link href="/" className="px-2 flex items-center justify-center">
+                  <img src="/img/hat.svg" alt="logo"
+                  className="w-10 h-10 object-cover inline-block" />
+                  <h2 className="ml-1 text-xl md:text-3xl tracking-wider
+                  text-green-600 tracking-wider hidden sm:inline-block my-font">foodie</h2>			  
+               </Link>
+            </nav>
             
-            <div className="w-2/12 hidden md:block">
-                 <Link href="/categories" className="px-2 text-xl md:text-lg text-center tracking-wider block">
-                    Categories
-                 </Link>
-            </div>
-          </header> );
-    
-  };
-  
-  export default Header;
+         <div className="w-9/12 md:w-6/12 px-2 md:px-4">
+         { /* search bar */ }
+         <Search />
+         </div>
+         
+         <div className="w-2/12 hidden md:block">
+               <Link href="/categories" className="px-2 text-lg text-center tracking-wider block">
+                  Categories
+               </Link>
+         </div>
+     </header> );
+
+
+
+};
+
+export default Header;
