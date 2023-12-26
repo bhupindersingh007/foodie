a025540a48ca0b89b62e3c0025e34acb454469f2
Bhupinder Singh <bs082045@gmail.com>
2023-12-26 16:31:25 -0500

    addd transition on card hover

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 const Card = (props : CardProps) => {
 
 		
-     return <article className="border rounded p-1 hover:shadow hover:border-0">
+     return <article className="border rounded p-1 hover:shadow hover:border-0 transition-shadow ease-in-out duration-150">
             <img src={props.mealImg} alt={props.mealName} 
 			className="w-full object-cover rounded lozad" />
             <h3 className="text-gray-700 text-center text-lg px-3 pt-3 pb-2 truncate">{props.mealName}</h3>
