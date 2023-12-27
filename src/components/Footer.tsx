e70d4cb7f244bb4f298ff8778d3ad8531bb174fb
Bhupinder Singh <bs082045@gmail.com>
2023-12-26 21:05:59 -0500

    fix attribute jsx syntax error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
          <svg viewBox="0 0 24 24" width="24" height="24"
             className="h-4 w-4 ml-1 inline-block align-baseline text-red-500 mb-0.5"
-            stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
             </path>
          </svg>
