05a80bb0968842879459fb1f51a66385bf48924b
Bhupinder Singh <bs082045@gmail.com>
2023-12-27 15:30:20 -0500

    list all search results and make suggestion box of fixed height with scrollbar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -66,7 +66,6 @@ class Search extends Component<{}, SearchState> {
 
     render() {
 
-        let classes = 'shadow w-full rounded text-left absolute bg-white mt-2';
 
         return (
 
@@ -87,7 +86,7 @@ class Search extends Component<{}, SearchState> {
                 {
                     this.state.query &&
                     <SuggestionBox recipes={this.state.recipes} 
-                    classes={classes} handleClick={this.handleClick} />
+                    handleClick={this.handleClick} />
 
                 }
             </div>
@@ -101,9 +100,12 @@ class Search extends Component<{}, SearchState> {
 
 function SuggestionBox(props: any) {
 
+    let classes = 'shadow w-full rounded text-left absolute bg-white mt-2 max-h-80 overflow-y-auto';
+
+
     if (!props.recipes) {
 
-        return <ul className={props.classes}>
+        return <ul className={classes}>
             {
                 <li className="px-2 py-1 text-gray-800 hover:bg-gray-100">
                     <p className="text-gray-500 ml-4">No such recipe</p>
@@ -115,15 +117,15 @@ function SuggestionBox(props: any) {
 
     return (
 
-        <ul className={props.classes}>
-            { props.recipes.splice(0, 5).map((recipe: any) => (
-                <li key={recipe.idMeal} className="px-2 py-1 text-gray-800 hover:bg-gray-100" onClick={props.handleClick}>
-                    <Link href="/recipes/[id]" as={`/recipes/${recipe.idMeal}`} className="flex items-center">
-                        <img src={recipe.strMealThumb} alt="image" className="w-12 h-12 rounded" />
-                        <p className="text-gray-800 ml-4">{recipe.strMeal}</p>
-                    </Link>
-                </li>)) }
-        </ul>
+            <ul className={classes}>
+                { props.recipes.map((recipe: any) => (
+                    <li key={recipe.idMeal} className="px-2 py-1 text-gray-800 hover:bg-gray-100" onClick={props.handleClick}>
+                        <Link href="/recipes/[id]" as={`/recipes/${recipe.idMeal}`} className="flex items-center">
+                            <img src={recipe.strMealThumb} alt="image" className="w-12 h-12 rounded" />
+                            <p className="text-gray-800 ml-4">{recipe.strMeal}</p>
+                        </Link>
+                    </li>)) }
+            </ul>
     );
 
 }
