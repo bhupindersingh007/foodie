766505d93fcc87f39932f5a72c73ddb7ff116b0f
Bhupinder Singh <bs082045@gmail.com>
2023-12-28 20:27:26 -0500

    show alert message when user bookmark a recipe

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -1,27 +1,29 @@
 'use client'
 
 import { useState, useEffect } from "react";
+import Alert from "./Alert";
 
 type BookmarkButtonProps = {
-    recipeId : number
+    recipeId: number
 }
 
-export default function BookmarkButton(props : BookmarkButtonProps) {
+export default function BookmarkButton(props: BookmarkButtonProps) {
 
     const [isBookmarked, setBookmark] = useState(false);
+    const [showAlert, setAlert] = useState(false);
 
+    const hideAlert = () => setTimeout(() => setAlert(false), 2000)
 
-
-    useEffect(()=>{
+    useEffect(() => {
 
         const recipeId = props.recipeId.toString();
 
 
-        if(localStorage.bookmarks){
+        if (localStorage.bookmarks) {
 
             JSON.parse(localStorage.bookmarks).indexOf(recipeId) != -1
-            ? setBookmark(true)
-            : setBookmark(false);
+                ? setBookmark(true)
+                : setBookmark(false);
 
         }
 
@@ -34,11 +36,13 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
 
         // no bookmarks
 
-        if(!localStorage.bookmarks){
+        if (!localStorage.bookmarks) {
 
 
             localStorage.setItem('bookmarks', JSON.stringify([recipeId]));
             setBookmark(true);
+            setAlert(true);
+            hideAlert();
             return;
 
         }
@@ -47,13 +51,13 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
 
         // remove, recipe already in bookmarks
 
-        if(bookmarks.indexOf(recipeId) != -1){
+        if (bookmarks.indexOf(recipeId) != -1) {
 
-          bookmarks = bookmarks.filter((bookmark : string) => bookmark != recipeId);
-          localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-          setBookmark(false);
-          return;
-            
+            bookmarks = bookmarks.filter((bookmark: string) => bookmark != recipeId);
+            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+            setBookmark(false);
+            return;
+
         }
 
         // add new recipe in bookmarks  
@@ -61,6 +65,8 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
         bookmarks.push(recipeId);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
         setBookmark(true);
+        setAlert(true);
+        hideAlert();
 
     };
 
@@ -68,12 +74,18 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
 
     return (
 
-        <button className="text-gray-800 me-2" onClick={ handleClick }>
-        <svg className={ isBookmarked ? 'fill-green-600 stroke-green-600' : '' }
-        viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" 
-        strokeLinecap="round" strokeLinejoin="round">
-            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
-        </button>
+        <>
+            {showAlert ? <Alert /> : ''}
+
+            <button className="text-gray-800 me-2" onClick={handleClick}>
+                <svg className={isBookmarked ? 'fill-green-600 stroke-green-600' : ''}
+                    viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none"
+                    strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                </svg>
+            </button>
+        </>
+
 
     );
 
