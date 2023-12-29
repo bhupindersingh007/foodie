ac8d4296733e3c3502f8cc1475610656b2599b29
Bhupinder Singh <bs082045@gmail.com>
2023-12-28 19:48:41 -0500

    make bookmark button stateful to keep track is recipe bookmarked

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -1,10 +1,31 @@
 'use client'
 
+import { useState, useEffect } from "react";
+
 type BookmarkButtonProps = {
     recipeId : number
 }
 
 export default function BookmarkButton(props : BookmarkButtonProps) {
+
+    const [isBookmarked, setBookmark] = useState(false);
+
+
+
+    useEffect(()=>{
+
+        const recipeId = props.recipeId.toString();
+
+
+        if(localStorage.bookmarks){
+
+            JSON.parse(localStorage.bookmarks).indexOf(recipeId) != -1
+            ? setBookmark(true)
+            : setBookmark(false);
+
+        }
+
+    })
 
 
     const handleClick = () => {
@@ -15,7 +36,9 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
 
         if(!localStorage.bookmarks){
 
+
             localStorage.setItem('bookmarks', JSON.stringify([recipeId]));
+            setBookmark(true);
             return;
 
         }
@@ -25,9 +48,10 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
         // remove, recipe already in bookmarks
 
         if(bookmarks.indexOf(recipeId) != -1){
-            
+
           bookmarks = bookmarks.filter((bookmark : string) => bookmark != recipeId);
           localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+          setBookmark(false);
           return;
             
         }
@@ -36,6 +60,7 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
 
         bookmarks.push(recipeId);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        setBookmark(true);
 
     };
 
@@ -44,7 +69,8 @@ export default function BookmarkButton(props : BookmarkButtonProps) {
     return (
 
         <button className="text-gray-800 me-2" onClick={ handleClick }>
-        <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" 
+        <svg className={ isBookmarked ? 'fill-green-600 stroke-green-600' : '' }
+        viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" 
         strokeLinecap="round" strokeLinejoin="round">
             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
         </button>
