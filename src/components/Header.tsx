4138aa7b26ee96c63a243bcd6a629df1cf864b0f
Bhupinder Singh <bs082045@gmail.com>
2023-12-31 16:22:41 -0500

    make searchbar toggle feature for small screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,59 @@
+'use client'
+
 import Link from "next/link";
 import Search from "./Search";
+import { useState } from "react";
 
 export default function Header() {
+
+   const [showSearch, setSearch] = useState(false);
 
 
    return (
 
       <header className="sticky left-0 top-0 bg-white border-b print:hidden">
 
-         <nav className="flex flex-wrap items-center py-3.5 md:w-11/12 md:mx-auto">
+         <nav className="flex flex-wrap items-center justify-between py-3.5 md:w-11/12 md:mx-auto">
 
-            <div className="w-3/12 hidden md:block px-2 md:px-4 tracking-wider font-semibold text-gray-800">
+            <div className="md:w-3/12 px-2 md:px-4 tracking-wider font-semibold text-gray-800">
                <Link href="/" className="px-2 flex items-center">
                   <img src="/img/hat.svg" alt="logo" className="w-10 h-10 object-cover inline-block" />
-                  <h2 className="ml-1 text-xl md:text-2xl tracking-wider
-                  text-green-600 tracking-wider hidden sm:inline-block my-font">foodie</h2>
+                  <h2 className="ml-1 text-2xl tracking-wider
+                  text-green-600 tracking-wider">foodie</h2>
                </Link>
             </div>
 
-            <div className="w-full md:w-6/12 px-2 md:px-4">
+            {/* search for medium and large screens */}
+            <div className="w-full md:w-6/12 px-2 md:px-4 hidden md:block">
                <Search />
             </div>
 
-            <div className="w-3/12 hidden md:block text-center">
+            {/* search for small screens */}
+
+            { showSearch ? <div className="w-full md:hidden order-last md:order-none mt-2.5 px-2"><Search /></div> : '' }
+
+            <div className="md:w-3/12 text-center pe-4 md:pe-0">
                
                {/* bookmarks */}
 
                <Link href="/bookmarks" className="inline-flex items-center text-gray-700 mt-1">
                   
-                  <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" 
+                  <svg viewBox="0 0 24 24" className="w-6 h-6 md:w-5 md:h-5" stroke="currentColor" strokeWidth="2" fill="none" 
                   strokeLinecap="round" strokeLinejoin="round">
                      <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                   </svg>
                   
-                  <span className="ms-1">My Bookmarks</span>
+                  <span className="ms-1 hidden md:inline-block">My Bookmarks</span>
 
                </Link>
+
+               <button className="text-gray-700 ms-1 md:hidden" onClick={ ()=> setSearch(!showSearch) }>
+                  <svg className="text-gray-700 w-6 h-6 md:w-5 md:h-5"
+                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+                     <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 
+                  16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
+                  </svg>
+               </button>
 
             </div>
          </nav>
