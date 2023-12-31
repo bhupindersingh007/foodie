859f463bb57e6b6c5375c176c3c4103166704672
Bhupinder Singh <bs082045@gmail.com>
2023-12-31 18:54:59 -0500

    use env variables for app url

--- src/app/api/lookup/route.ts
+++ src/app/api/lookup/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const id = searchParams.get('id');
 
-    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const res = await fetch(`${process.env.API_URL}/lookup.php?i=${id}`);
 
     if (!res.ok) {
         throw new Error('Failed to fetch data')
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get('query');
 
-    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const res = await fetch(`${process.env.API_URL}/search.php?s=${query}`);
 
     if (!res.ok) {
         throw new Error('Failed to fetch data')
