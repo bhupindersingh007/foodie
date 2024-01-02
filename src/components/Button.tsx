75d6eef9856cc17ce288e07d51735d76995a0ba8
Bhupinder Singh <bs082045@gmail.com>
2024-01-02 17:33:37 -0500

    add right arrow icon in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,8 +13,15 @@ export default function Button(props: ButtonProps) {
         <Link href={props.href}
             className="bg-green-600 text-white text-lg px-9 py-3 rounded-full 
             transition-shadow transition-colors duration-100 tracking-wider 
-            hover:bg-green-700 hover:shadow-sm">
-            {props.title}
+            hover:bg-green-700 hover:shadow-sm inline-flex items-center">
+            <span className="me-0.5">{props.title}</span>
+
+            <svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none"
+             stroke-linecap="round" stroke-linejoin="round">
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+            </svg>
+
         </Link>
 
     );
