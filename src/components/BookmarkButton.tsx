575da201a7b21d5fe5207473a056d6333d9ab512
Bhupinder Singh <bs082045@gmail.com>
2024-01-02 17:22:28 -0500

    increase size of print & bookmark icons on small screens

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -67,6 +67,7 @@ export default function BookmarkButton(props: BookmarkButtonProps) {
     };
 
 
+    let classNames = isBookmarked ? 'fill-green-600 stroke-green-600 w-6 h-6 md:w-5 md:h-5' : 'w-6 h-6 md:w-5 md:h-5';
 
     return (
 
@@ -74,8 +75,8 @@ export default function BookmarkButton(props: BookmarkButtonProps) {
             {showAlert ? <Alert /> : ''}
 
             <button className="text-gray-800 me-2" onClick={handleClick} title="Bookmark">
-                <svg className={isBookmarked ? 'fill-green-600 stroke-green-600' : ''}
-                    viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none"
+                <svg className={classNames}
+                    viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none"
                     strokeLinecap="round" strokeLinejoin="round">
                     <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                 </svg>
