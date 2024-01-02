149bdce49180685a72b586d94813606ff8596ce8
Bhupinder Singh <bs082045@gmail.com>
2024-01-02 17:12:52 -0500

    fix categories grid for large screens

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -33,7 +33,7 @@ export default async function Categories() {
             
             <div className="flex flex-wrap md:w-11/12 md:mx-auto mb-6 px-2">
                 { data.categories.map((category : Category) => (
-                    <div key={category.idCategory}  className="w-1/2 sm:w-4/12 lg:w-2/12 mb-4 px-2">
+                    <div key={category.idCategory}  className="w-1/2 sm:w-4/12 lg:w-3/12 mb-4 px-2">
                         <Link href="/dishes/[name]" as={`/dishes/${category.strCategory}`}>
                             <Card mealName={category.strCategory} mealImg={category.strCategoryThumb} />
                         </Link>
